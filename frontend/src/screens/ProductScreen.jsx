e234468c1e51d07c1dc6038d72d59bc034665fbb
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-28 13:42:11 +1000

    Fix bugs for product review

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -162,6 +162,7 @@ const ProductScreen = () => {
 						</Card>
 					</Col>
 				</Row>
+
 				<Row className='review'>
 					<Col md={6}>
 						<h2>Reviews</h2>
@@ -177,71 +178,79 @@ const ProductScreen = () => {
 									<p>{review.comment}</p>
 								</ListGroup.Item>
 							))}
-						</ListGroup>
-						<ListGroup.Item>
-							<h2>Write a Customer Review</h2>
+							<ListGroup.Item>
+								<h2>Write a Customer Review</h2>
 
-							{loadingProductReview && <Loader />}
+								{loadingProductReview && <Loader />}
 
-							{userInfo ? (
-								<Form onSubmit={handleSubmit}>
-									<Form.Group
-										controlId='rating'
-										className='my-2'
-									>
-										<Form.Label>Rating</Form.Label>
-										<Form.Control
-											as='select'
-											value={rating}
-											onChange={(e) =>
-												setRating(
-													Number(e.target.value)
-												)
-											}
+								{userInfo ? (
+									<Form onSubmit={handleSubmit}>
+										<Form.Group
+											controlId='rating'
+											className='my-2'
 										>
-											<option value=''>Select...</option>
-											<option value='1'>1 - Poor</option>
-											<option value='2'>2 - Fair</option>
-											<option value='3'>3 - Good</option>
-											<option value='4'>
-												4 - Very Good
-											</option>
-											<option value='5'>
-												5 - Excellent
-											</option>
-										</Form.Control>
-									</Form.Group>
+											<Form.Label>Rating</Form.Label>
+											<Form.Control
+												as='select'
+												value={rating}
+												onChange={(e) =>
+													setRating(
+														Number(e.target.value)
+													)
+												}
+											>
+												<option value=''>
+													Select...
+												</option>
+												<option value='1'>
+													1 - Poor
+												</option>
+												<option value='2'>
+													2 - Fair
+												</option>
+												<option value='3'>
+													3 - Good
+												</option>
+												<option value='4'>
+													4 - Very Good
+												</option>
+												<option value='5'>
+													5 - Excellent
+												</option>
+											</Form.Control>
+										</Form.Group>
 
-									<Form.Group
-										controlId='comment'
-										className='my-2'
-									>
-										<Form.Label>Comment</Form.Label>
-										<Form.Control
-											as='textarea'
-											row='3'
-											value={comment}
-											onChange={(e) =>
-												setComment(e.target.value)
-											}
-										></Form.Control>
-									</Form.Group>
+										<Form.Group
+											controlId='comment'
+											className='my-2'
+										>
+											<Form.Label>Comment</Form.Label>
+											<Form.Control
+												as='textarea'
+												row='3'
+												value={comment}
+												onChange={(e) =>
+													setComment(e.target.value)
+												}
+											></Form.Control>
+										</Form.Group>
 
-									<Button
-										disabled={loadingProductReview}
-										type='submit'
-										variant='primary'
-									>
-										Submit
-									</Button>
-								</Form>
-							) : (
-								<Message variant='info'>
-									Please <Link to='/login'>sign in</Link> to
-									write a review{' '}
-								</Message>
-							)}
-						</ListGroup.Item>
+										<Button
+											disabled={loadingProductReview}
+											type='submit'
+											variant='primary'
+										>
+											Submit
+										</Button>
+									</Form>
+								) : (
+									<Message variant='info'>
+										Please <Link to='/login'>sign in</Link>{' '}
+										to write a review{' '}
+									</Message>
+								)}
+							</ListGroup.Item>
+						</ListGroup>
 					</Col>
 				</Row>
 			</>
